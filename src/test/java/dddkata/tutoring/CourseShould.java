67c0d9fec0da602fb1cac846e617c046d440e410
Ben Wu <wubin28@gmail.com>
2015-06-24 06:49:18 +0800

    Added Assert for test CourseShould.tellMeTheEndDate().

---
+++ src/test/java/dddkata/tutoring/CourseShould.java
@@ -0,0 +1,14 @@
+package dddkata.tutoring;
+
+import org.junit.Test;
+
+/**
+ * Created by twer on 6/23/15.
+ */
+public class CourseShould {
+    @Test
+    public void tellMeTheEndDate() {
+        // Assert
+        assertEquals("2015.04.29", course.getDateRange.getEndDate());
+    }
+}
