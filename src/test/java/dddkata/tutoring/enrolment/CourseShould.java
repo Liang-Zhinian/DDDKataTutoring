5ec57b2330b656818b56bd49db1ced6ca937d58d
Ben Wu <wubin28@gmail.com>
2015-06-24 16:25:00 +0800

    Added Assert for test CourseShould.tellMeTheKidsEnrolled().

--- src/test/java/dddkata/tutoring/enrolment/CourseShould.java
+++ src/test/java/dddkata/tutoring/enrolment/CourseShould.java
@@ -1,10 +1,19 @@
 package dddkata.tutoring.enrolment;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by twer on 6/24/15.
  */
 public class CourseShould {
-    // TODO: tellMeTheKidsEnrolled
+    @Test
+    public void tellMeTheKidsEnrolled() {
+        // Assert
+        assertEquals("田斯斯_女_3岁 马一达_男_3岁 ", course.getKids());
+    }
+
     // TODO: tellMeTheGuardianForAKid
     // TODO: tellMeTheDateWhenAKidsFeeWasPaid
 }
