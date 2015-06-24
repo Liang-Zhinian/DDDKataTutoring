14cb89ae63357ae81bbbfcbf91570ad4fabda1e4
Ben Wu <wubin28@gmail.com>
2015-06-24 11:59:28 +0800

    Implemented method DateRange.getEndDate() using a hardcording start date.

--- src/main/java/dddkata/tutoring/scheduling/DateRange.java
+++ src/main/java/dddkata/tutoring/scheduling/DateRange.java
@@ -1,14 +1,24 @@
 package dddkata.tutoring.scheduling;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Created by twer on 6/24/15.
  */
 public class DateRange {
-    public DateRange(String dayOfWeek, String startTime, String endTime, String startDate, int times) {
+    private int times;
 
+    public DateRange(String dayOfWeek, String startTime, String endTime, String startDate, int times) {
+        this.times = times;
     }
 
     public String getEndDate() {
-        return "2015.04.29";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        LocalDateTime startTime = LocalDateTime.of(2015, Month.FEBRUARY, 11, 18, 0);
+        LocalDateTime endTime = startTime.plusWeeks(this.times - 1);
+        String endDate = endTime.format(formatter);
+        return endDate;
     }
 }
