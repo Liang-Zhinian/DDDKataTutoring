a1feed9e928c5209462f9bf0995da3ac512d6e79
Ben Wu <wubin28@gmail.com>
2015-06-24 15:48:37 +0800

    Added 3 TODOs for class CourseShould.

---
+++ src/test/java/dddkata/tutoring/enrolment/CourseShould.java
@@ -0,0 +1,10 @@
+package dddkata.tutoring.enrolment;
+
+/**
+ * Created by twer on 6/24/15.
+ */
+public class CourseShould {
+    // TODO: tellMeTheKidsEnrolled
+    // TODO: tellMeTheGuardianForAKid
+    // TODO: tellMeTheDateWhenAKidsFeeWasPaid
+}
