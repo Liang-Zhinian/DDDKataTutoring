b3d7b53dad258590d9693f1b0a97798172f8068e
Ben Wu <wubin28@gmail.com>
2015-06-24 13:05:05 +0800

    Implemented Teacher factory.

--- src/main/java/dddkata/tutoring/crudmanagement/Teacher.java
+++ src/main/java/dddkata/tutoring/crudmanagement/Teacher.java
@@ -4,7 +4,20 @@ package dddkata.tutoring.crudmanagement;
  * Created by twer on 6/24/15.
  */
 public class Teacher {
-    public Teacher(String name, String mobile) {
+    private String mobile;
+    private String name;
 
+    private Teacher() {
+
+    }
+
+    private Teacher(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public static Teacher create(String name, String mobile) {
+        Teacher teacher = new Teacher(name, mobile);
+        return teacher;
     }
 }
