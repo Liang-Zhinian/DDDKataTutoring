99eb480f0ea9b90f5b8ec06d9c0acacbb96b3ea9
Ben Wu <wubin28@gmail.com>
2015-06-25 09:32:44 +0800

    Wrote intent code for test enrolment.CourseShould.tellMeTheKidsEnrolled().

--- src/test/java/dddkata/tutoring/enrolment/CourseShould.java
+++ src/test/java/dddkata/tutoring/enrolment/CourseShould.java
@@ -11,7 +11,23 @@ public class CourseShould {
     @Test
     public void tellMeTheKidsEnrolled() {
         // Arrange
-        dddkata.tutoring.enrolment.Course course = CourseRepository.getByName("美术预科");
+        dddkata.tutoring.enrolment.Course course = dddkata.tutoring.enrolment.Course.newInstance("美术预科");
+
+        List<Kid> kids = new ArrayList<Kid>();
+
+        Kid tianSiSi = Kid.newInstance("田斯斯", "女", "3");
+        Guardian tianSiSiMom = Guardian.newInstance("Mom", "13921223456");
+        tianSiSi.setGuardian(tianSiSiMom);
+        Fee tianSiSiFee = Fee.newInstance("2015.01.15", 3000);
+        tianSiSi.setFee(tianSiSiFee);
+        course.addKid(tianSiSi);
+
+        Kid maYiDa = Kid.newInstance("马一达", "男", "3岁");
+        Guardian maYiDaDad = Guardian.newInstance("Dad", "13921223211");
+        maYiDa.setGuardian(maYiDaDad);
+        Fee maYiDaFee = Fee.newInstance("2015.01.18", 3000);
+        maYiDa.setFee(maYiDaFee);
+        course.addKid(maYiDa);
 
         // Act & Assert
         assertEquals("田斯斯_女_3岁 马一达_男_3岁 ", course.getKids());
