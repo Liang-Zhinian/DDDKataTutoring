49302aedc49121af2898827b7936d81e62b683eb
Ben Wu <wubin28@gmail.com>
2015-06-25 12:56:06 +0800

    Added 6 TODOs for class enrolment.CourseRepositoryShould.

---
+++ src/test/java/dddkata/tutoring/enrolment/CourseRepositoryShould.java
@@ -0,0 +1,13 @@
+package dddkata.tutoring.enrolment;
+
+/**
+ * Created by twer on 6/25/15.
+ */
+public class CourseRepositoryShould {
+    // TODO: enrolAKidForACourse
+    // TODO: listAllKidsForACourse
+    // TODO: updateKidInfoForACourse
+    // TODO: deleteKidFromACourse
+    // TODO: updateGuardianInfoOfAKidForACourse
+    // TODO: updateFeeInfoOfAKidForACourse
+}
