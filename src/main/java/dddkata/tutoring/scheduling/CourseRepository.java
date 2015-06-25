e6759793f73e41f4414bae0df659521234e82cef
Ben Wu <wubin28@gmail.com>
2015-06-25 10:22:51 +0800

    Implemented method scheduling.CourseRepository.newInstance() and made all tests passed.

--- src/main/java/dddkata/tutoring/scheduling/CourseRepository.java
+++ src/main/java/dddkata/tutoring/scheduling/CourseRepository.java
@@ -10,11 +10,19 @@ public class CourseRepository {
 
     private DataService dataService = DataService.getInstance();
 
+    private CourseRepository() {
+
+    }
+
     public void save(String name, Course course) {
         dataService.saveSchedulingCourse(name, course);
     }
 
     public Course retrieve(String name) {
         return dataService.retrieveSchedulingCourse(name);
+    }
+
+    public static CourseRepository newInstance() {
+        return new CourseRepository();
     }
 }
