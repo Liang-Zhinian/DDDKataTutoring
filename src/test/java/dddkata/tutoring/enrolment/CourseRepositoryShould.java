7b8d3e05b704fddc818222d5db813eb9a807e483
Ben Wu <wubin28@gmail.com>
2015-06-25 13:01:12 +0800

    Added Assert for test enrolment.CourseRepositoryShould.enrolAKidForACourse().

--- src/test/java/dddkata/tutoring/enrolment/CourseRepositoryShould.java
+++ src/test/java/dddkata/tutoring/enrolment/CourseRepositoryShould.java
@@ -1,10 +1,19 @@
 package dddkata.tutoring.enrolment;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by twer on 6/25/15.
  */
 public class CourseRepositoryShould {
-    // TODO: enrolAKidForACourse
+    @Test
+    public void enrolAKidForACourse() {
+        // Assert
+        assertEquals(kidToBeEnrolled, kidEnrolled);
+    }
+
     // TODO: listAllKidsForACourse
     // TODO: updateKidInfoForACourse
     // TODO: deleteKidFromACourse
