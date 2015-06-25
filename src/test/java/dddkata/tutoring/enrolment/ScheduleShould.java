dc3c127b3a3a68393293d3e70d7684ec8ed34ef6
Ben Wu <wubin28@gmail.com>
2015-06-25 16:47:46 +0800

    Added TODOs for scheduling.ScheduleShould and enrolment.ScheduleShould.

---
+++ src/test/java/dddkata/tutoring/enrolment/ScheduleShould.java
@@ -0,0 +1,8 @@
+package dddkata.tutoring.enrolment;
+
+/**
+ * Created by twer on 6/25/15.
+ */
+public class ScheduleShould {
+    // TODO: let_me_know_if_two_courses_are_arranged_for_one_kid_within_the_same_time_range
+}
---
+++ src/test/java/dddkata/tutoring/scheduling/ScheduleShould.java
@@ -0,0 +1,10 @@
+package dddkata.tutoring.scheduling;
+
+/**
+ * Created by twer on 6/25/15.
+ */
+public class ScheduleShould {
+    // TODO: let_me_know_if_two_courses_are_arranged_in_the_same_room_within_the_same_time_range
+    // TODO: let_me_know_if_two_courses_are_arranged_for_one_teacher_within_the_same_time_range
+
+}
