387144a07e68c2ae16b8317e9b7b5bec56f3735d
Ben Wu <wubin28@gmail.com>
2015-06-25 17:08:54 +0800

    Added Assert for test scheduling.ScheduleShould.let_me_know_if_two_courses_are_arranged_in_the_same_room_within_the_same_time_range().

--- src/test/java/dddkata/tutoring/scheduling/ScheduleShould.java
+++ src/test/java/dddkata/tutoring/scheduling/ScheduleShould.java
@@ -1,10 +1,18 @@
 package dddkata.tutoring.scheduling;
 
+import org.junit.Test;
+
 /**
  * Created by twer on 6/25/15.
  */
 public class ScheduleShould {
-    // TODO: let_me_know_if_two_courses_are_arranged_in_the_same_room_within_the_same_time_range
+    @Test
+    public void let_me_know_if_two_courses_are_arranged_in_the_same_room_within_the_same_time_range() {
+        // Assert
+        assertEquals(coursePreparatory, conflictedCourses.get("美术预科"));
+        assertEquals(courseHandwriting, conflictedCourses.get("书法"));
+    }
+
     // TODO: let_me_know_if_two_courses_are_arranged_for_one_teacher_within_the_same_time_range
 
 }
