215ba548d7b2272eaf1ca69a214773b3d43561dd
Ben Wu <wubin28@gmail.com>
2015-06-26 17:20:16 +0800

    Wrote intent code for method enrolment.Schedule.isOverlapped().

--- src/main/java/dddkata/tutoring/enrolment/Schedule.java
+++ src/main/java/dddkata/tutoring/enrolment/Schedule.java
@@ -28,23 +28,27 @@ public class Schedule {
         return conflictedCourses;
     }
 
-    public void addKid(Kid kid, String courseString) {
+    public void addKid(Kid kid, String courseString, dddkata.tutoring.scheduling.Schedule scheduleInScheduling) {
         if (this.kidAndEnrolledCourses.keySet().contains(kid.getName())
                 && this.kidAndEnrolledCourses.get(kid.getName()).size() > 0) {
-            markTwoConflictedCourses(courseString);
+            markTwoConflictedCourses(courseString, scheduleInScheduling);
         }
         this.courses.get(courseString).addKid(kid);
     }
 
-    private void markTwoConflictedCourses(String courseStringJustAdded) {
+    private void markTwoConflictedCourses(String courseStringJustAdded, dddkata.tutoring.scheduling.Schedule scheduleInScheduling) {
         conflictedCourses = new HashMap<>();
         for(Map.Entry<String, Course> courseWithName : courses.entrySet()) {
-            if (isOverlapped(courseWithName.getValue(), this.courses.get(courseStringJustAdded))) {
+            if (isOverlapped(courseWithName.getValue(), this.courses.get(courseStringJustAdded), scheduleInScheduling)) {
                 conflictedCourses.put(courseWithName.getKey(), courseWithName.getValue());
                 conflictedCourses.put(this.courses.get(courseStringJustAdded).getName()
                 , this.courses.get(courseStringJustAdded));
                 break;
             }
         }
+    }
+
+    private boolean isOverlapped(Course thisCourse, Course thatCourse, dddkata.tutoring.scheduling.Schedule scheduleInScheduling) {
+        return scheduleInScheduling.isOverlapped(thisCourse, thatCourse);
     }
 }
