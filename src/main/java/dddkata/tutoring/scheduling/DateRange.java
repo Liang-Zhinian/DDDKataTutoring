04bf9c9c12f257ff8bb2c17b04d40d2ecbb702ee
Ben Wu <wubin28@gmail.com>
2015-06-26 10:34:17 +0800

    Wrote intent code for method scheduling.DateRange.conflictsWith().

--- src/main/java/dddkata/tutoring/scheduling/DateRange.java
+++ src/main/java/dddkata/tutoring/scheduling/DateRange.java
@@ -2,35 +2,70 @@ package dddkata.tutoring.scheduling;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Created by twer on 6/24/15.
  */
 public class DateRange {
     private final String startDate; // 2015.02.11
-    private final String startTime; // 18
+    private final String startTime; // e.g. 18:00
+    private final String endTime; // e.g. 19:30
     private int times;
+    private String dayOfWeek;
 
     public DateRange(String dayOfWeek, String startTime, String endTime, String startDate, int times) {
         this.times = times;
         this.startDate = startDate;
         this.startTime = startTime;
+        this.dayOfWeek = dayOfWeek;
+        this.endTime = endTime;
     }
 
     public String getEndDate() {
-        DateTimeFormatter startTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
-        LocalDateTime startTime = LocalDateTime.parse(this.startDate + " " + this.startTime, startTimeFormatter);
+        DateTimeFormatter yyyyMMddHHmmFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+        LocalDateTime firstStartTime = LocalDateTime.parse(this.startDate + " " + this.startTime, yyyyMMddHHmmFormatter);
+        LocalDateTime firstEndTime = LocalDateTime.parse(this.startDate + " " + this.endTime, yyyyMMddHHmmFormatter);
 
-        DateTimeFormatter endTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        LocalDateTime endTime = startTime.plusWeeks(this.times - 1);
-        String endDate = endTime.format(endTimeFormatter);
+        DateTimeFormatter yyyyMMddFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        LocalDateTime lastEndTime = firstStartTime.plusWeeks(this.times - 1).plusMinutes(ChronoUnit.MINUTES.between(firstStartTime, firstEndTime));
+        String endDate = lastEndTime.format(yyyyMMddFormatter);
         return endDate;
     }
 
     public boolean conflictsWith(DateRange dateRange) {
-        if (isTheSameDayOfWeek(dateRange.getDayOfWeek()) && isDateOverlapped(dateRange) && isTimeOverlapped(dateRange)) {
+        if (isTheSameDayOfWeek(dateRange.getDayOfWeek()) && isDateOverlapped(dateRange)
+                && isTimeOverlapped(dateRange)) {
             return true;
         }
         return false;
+    }
+
+    private boolean isTimeOverlapped(DateRange dateRange) {
+        if (this.startTime.equals(dateRange.getStartTime()) && this.endTime.equals(dateRange.getEndTime())) {
+            return true;
+        }
+        if (isThisEndTimeEarlyThanThatStartTime(dateRange) || isThisStartTimeLaterThanThatEndTime(dateRange)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isDateOverlapped(DateRange dateRange) {
+        if (this.startDate.equals(dateRange.getStartDate()) && isTheSameEndDate(dateRange)) {
+            return true;
+        }
+        if (isThisEndDateEarlyThanThatStartDate(dateRange) || isThisStartDateLaterThanThatEndDate(dateRange)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isTheSameDayOfWeek(String dayOfWeek) {
+        return this.dayOfWeek.equals(dayOfWeek);
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
     }
 }
