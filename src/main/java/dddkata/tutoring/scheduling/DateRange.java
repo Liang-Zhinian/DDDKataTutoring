e2d2bd551a49cc7bfbb4c177b6e2b102781d9369
Ben Wu <wubin28@gmail.com>
2015-06-26 11:35:31 +0800

    Implemented methods DateRange.isThisEndDateEarlyThanThatStartDate() and DateRange.isThisStartDateLaterThanThatEndDate().

--- src/main/java/dddkata/tutoring/scheduling/DateRange.java
+++ src/main/java/dddkata/tutoring/scheduling/DateRange.java
@@ -1,6 +1,7 @@
 package dddkata.tutoring.scheduling;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
@@ -8,24 +9,24 @@ import java.time.temporal.ChronoUnit;
  * Created by twer on 6/24/15.
  */
 public class DateRange {
-    private final String startDate; // 2015.02.11
-    private final String startTime; // e.g. 18:00
-    private final String endTime; // e.g. 19:30
+    private final String startDateString; // 2015.02.11
+    private final String startTimeString; // e.g. 18:00
+    private final String endTimeString; // e.g. 19:30
     private int times;
     private String dayOfWeek;
 
-    public DateRange(String dayOfWeek, String startTime, String endTime, String startDate, int times) {
+    public DateRange(String dayOfWeek, String startTimeString, String endTimeString, String startDateString, int times) {
         this.times = times;
-        this.startDate = startDate;
-        this.startTime = startTime;
+        this.startDateString = startDateString;
+        this.startTimeString = startTimeString;
         this.dayOfWeek = dayOfWeek;
-        this.endTime = endTime;
+        this.endTimeString = endTimeString;
     }
 
     public String getEndDateString() {
         DateTimeFormatter yyyyMMddHHmmFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
-        LocalDateTime firstStartTime = LocalDateTime.parse(this.startDate + " " + this.startTime, yyyyMMddHHmmFormatter);
-        LocalDateTime firstEndTime = LocalDateTime.parse(this.startDate + " " + this.endTime, yyyyMMddHHmmFormatter);
+        LocalDateTime firstStartTime = LocalDateTime.parse(this.startDateString + " " + this.startTimeString, yyyyMMddHHmmFormatter);
+        LocalDateTime firstEndTime = LocalDateTime.parse(this.startDateString + " " + this.endTimeString, yyyyMMddHHmmFormatter);
 
         DateTimeFormatter yyyyMMddFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
         LocalDateTime lastEndTime = firstStartTime.plusWeeks(this.times - 1).plusMinutes(ChronoUnit.MINUTES.between(firstStartTime, firstEndTime));
@@ -42,7 +43,7 @@ public class DateRange {
     }
 
     private boolean isTimeOverlapped(DateRange dateRange) {
-        if (this.startTime.equals(dateRange.getStartTime()) && this.endTime.equals(dateRange.getEndTime())) {
+        if (this.startTimeString.equals(dateRange.getStartTime()) && this.endTimeString.equals(dateRange.getEndTime())) {
             return true;
         }
         if (isThisEndTimeEarlyThanThatStartTime(dateRange) || isThisStartTimeLaterThanThatEndTime(dateRange)) {
@@ -52,7 +53,7 @@ public class DateRange {
     }
 
     private boolean isDateOverlapped(DateRange dateRange) {
-        if (this.startDate.equals(dateRange.getStartDate()) && isTheSameEndDate(dateRange)) {
+        if (this.startDateString.equals(dateRange.getStartDateString()) && isTheSameEndDate(dateRange)) {
             return true;
         }
         if (isThisEndDateEarlyThanThatStartDate(dateRange) || isThisStartDateLaterThanThatEndDate(dateRange)) {
@@ -61,8 +62,19 @@ public class DateRange {
         return true;
     }
 
+    private boolean isThisStartDateLaterThanThatEndDate(DateRange dateRange) {
+        return convertDateStringToLocalDateTime(this.startDateString).toInstant(ZoneOffset.of("+08:00"))
+                .isAfter(dateRange.convertDateStringToLocalDateTime(dateRange.getEndDateString()).toInstant(ZoneOffset.of("+08:00")));
+    }
+
     private boolean isThisEndDateEarlyThanThatStartDate(DateRange dateRange) {
-        return getEndDateTime(getEndDateString()).before(dateRange.getEndDateTime(dateRange.getEndDateString()));
+        return convertDateStringToLocalDateTime(getEndDateString()).toInstant(ZoneOffset.of("+08:00"))
+                .isBefore(dateRange.convertDateStringToLocalDateTime(dateRange.getStartDateString()).toInstant(ZoneOffset.of("+08:00")));
+    }
+
+    private LocalDateTime convertDateStringToLocalDateTime(String dateString) {
+        DateTimeFormatter yyyyMMddHHmmFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+        return LocalDateTime.parse(dateString + " " + "00:00", yyyyMMddHHmmFormatter);
     }
 
     private boolean isTheSameEndDate(DateRange dateRange) {
@@ -77,7 +89,7 @@ public class DateRange {
         return dayOfWeek;
     }
 
-    public String getStartDate() {
-        return startDate;
+    public String getStartDateString() {
+        return startDateString;
     }
 }
