039118bb29f70e6e7b6e9dfa26f4c8eb78ec1cfc
Ben Wu <wubin28@gmail.com>
2015-06-26 15:26:09 +0800

    Updated the test name enrolment.ScheduleShould.let_me_know_if_a_kid_enrolled_a_course_which_have_a_time_conflict_with_another_course() and wrote Assert.

--- src/test/java/dddkata/tutoring/enrolment/ScheduleShould.java
+++ src/test/java/dddkata/tutoring/enrolment/ScheduleShould.java
@@ -1,8 +1,16 @@
 package dddkata.tutoring.enrolment;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by twer on 6/25/15.
  */
 public class ScheduleShould {
-    // TODO: let_me_know_if_two_courses_are_arranged_for_one_kid_within_the_same_time_range
+    @Test
+    public void let_me_know_if_a_kid_enrolled_a_course_which_have_a_time_conflict_with_another_course() {
+        // Assert
+        assertEquals(courseHandwriting, conflictedCourse);
+    }
 }
