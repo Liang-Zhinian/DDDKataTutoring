52869d2232733a55025a471e4f1459542bf4e989
Ben Wu <wubin28@gmail.com>
2015-07-10 10:54:50 +0800

    Implemented method DataService.getAllSchedulingCourses() and made all tests passed.

--- src/main/java/dddkata/tutoring/sharedService/DataService.java
+++ src/main/java/dddkata/tutoring/sharedService/DataService.java
@@ -1,9 +1,10 @@
 package dddkata.tutoring.sharedService;
 
 import dddkata.tutoring.enrolment.Kid;
-import dddkata.tutoring.scheduling.Course;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by twer on 6/25/15.
@@ -23,11 +24,11 @@ public class DataService {
         return singleton;
     }
 
-    public void saveSchedulingCourse(String name, Course course) {
+    public void saveSchedulingCourse(String name, dddkata.tutoring.scheduling.Course course) {
         this.schedulingCourses.put(name, course);
     }
 
-    public Course retrieveSchedulingCourse(String name) {
+    public dddkata.tutoring.scheduling.Course retrieveSchedulingCourse(String name) {
         return this.schedulingCourses.get(name);
     }
 
@@ -38,5 +39,9 @@ public class DataService {
     public Kid retrieveAKidForACourse(String courseName, String kidName) {
         Kid kid = this.enrolmentCourses.get(courseName).getKids().get(kidName);
         return kid;
+    }
+
+    public List<dddkata.tutoring.scheduling.Course> getAllSchedulingCourses() {
+        return new ArrayList<>(this.schedulingCourses.values());
     }
 }
