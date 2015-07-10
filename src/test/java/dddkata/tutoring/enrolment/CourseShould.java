cd44d203050ca30e4f621dc722452b5d4006be71
Ben Wu <wubin28@gmail.com>
2015-07-10 15:29:53 +0800

    Moved test help_miss_hehe_to_update_an_enrolment_entry_for_a_course from repository test class to enrolment.CourseShould and deleted the former.

--- src/test/java/dddkata/tutoring/enrolment/CourseShould.java
+++ src/test/java/dddkata/tutoring/enrolment/CourseShould.java
@@ -54,8 +54,30 @@ public class CourseShould {
         // Assert
         assertEquals(2, enrolmentRetrieved.size());
     }
-    
-    // TODO: help_miss_hehe_to_update_an_enrolment_entry_for_a_course
+
+    @Test
+    public void help_miss_hehe_to_update_an_enrolment_entry_for_a_course() {
+        // Arrange
+        CourseRepository repository =
+                CourseRepository.newInstance();
+        Course course = Course.newInstance("美术预科");
+        repository.addCourse(course.getName(), course);
+
+        Guardian tianSiSiMom = Guardian.newInstance("Mom", "13921223456");
+        Fee tianSiSiFee = Fee.newInstance("2015.02.15", 3000);
+        Enrolment tianSiSi = Enrolment.newInstance("田斯斯", "女", 3, tianSiSiMom, tianSiSiFee);
+
+        // Act
+        repository.enrolAKidForACourse("美术预科", tianSiSi);
+
+        Enrolment tianSiSiUpdated = Enrolment.newInstance("田斯斯", "女", 2, tianSiSiMom, tianSiSiFee);;
+        repository.enrolAKidForACourse("美术预科", tianSiSiUpdated);
+
+        // Assert
+        Enrolment enrolmentEnrolled = repository.retrieveAKidForACourse("美术预科", "田斯斯");
+        assertEquals(tianSiSiUpdated, enrolmentEnrolled);
+    }
+
     // TODO: help_miss_hehe_to_delete_an_enrolment_entry_from_a_course
     // TODO: help_miss_hehe_to_list_the_information_of_all_kids_enrolled_in_a_course
     // TODO: help_miss_hehe_to_get_contact_information_of_a_kid_in_a_course
