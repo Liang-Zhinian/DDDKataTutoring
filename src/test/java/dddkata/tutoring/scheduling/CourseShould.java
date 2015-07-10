e2395c633dd3792552e556ef1ce9bb050fa8694d
Ben Wu <wubin28@gmail.com>
2015-07-10 10:41:24 +0800

    Wrote intent code for test scheduling.CourseShould.help_miss_hehe_to_browse_all_courses_created().

--- src/test/java/dddkata/tutoring/scheduling/CourseShould.java
+++ src/test/java/dddkata/tutoring/scheduling/CourseShould.java
@@ -32,7 +32,36 @@ public class CourseShould {
         assertEquals(courseToBeCreated, courseRetrieved);
     }
 
-    // TODO: help_miss_hehe_to_browse_all_courses_created
+    @Test
+    public void help_miss_hehe_to_browse_all_courses_created() {
+        // Arrange
+        CourseRepository repository = CourseRepository.newInstance();
+
+        List<Teacher> teachers = new ArrayList<Teacher>();
+        teachers.add(Teacher.newInstance("幼幼", "13809878765"));
+        DateRange dateRange = new DateRange("Wednesday", "18:00", "19:30", "2015.02.11", 12);
+        Course courseToBeCreated = Course.newInstance("美术预科",
+                "针对2-3岁儿童心理、生理特点以及敏感期的发展特点，从最基础的看、摸、闻、听、尝（视觉、触觉、嗅觉、听觉、味觉）感觉入手，培养孩子最基础的",
+                "2～3岁",
+                3000, "达芬奇", teachers, dateRange);
+        repository.save("美术预科", courseToBeCreated);
+
+        teachers = new ArrayList<Teacher>();
+        teachers.add(Teacher.newInstance("杆子", "18976876567"));
+        dateRange = new DateRange("Thursday", "18:00", "19:30", "2015.02.12", 12);
+        courseToBeCreated = Course.newInstance("书法",
+                "书法的字体结构与笔画线条的教学，培养儿童对中国文化的理解与表达能力。书法是以书法技法学习为途径，通过对汉字艺术造型演化的了解，获得传统文",
+                "5～8岁",
+                2000, "毕加索", teachers, dateRange);
+        repository.save("书法", courseToBeCreated);
+
+        // Act
+        List<Course> coursesRetrieved = repository.getAllCourses();
+
+        // Assert
+        assertEquals(2, coursesRetrieved.size());
+    }
+
     // TODO: help_miss_hehe_to_update_a_course
     // TODO: help_miss_hehe_to_delete_a_course
     // TODO: help_miss_hehe_to_search_a_course
