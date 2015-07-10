a11018d3e3ed11a29733d6b53f72f74f8f378af7
Ben Wu <wubin28@gmail.com>
2015-07-10 10:30:32 +0800

    Moved a test from scheduling.CourseRepositoryShould to scheduling.CourseShould and renamed it to be help_miss_hehe_to_create_a_course().

--- src/test/java/dddkata/tutoring/scheduling/CourseRepositoryShould.java
+++ src/test/java/dddkata/tutoring/scheduling/CourseRepositoryShould.java
@@ -13,26 +13,6 @@ import static org.junit.Assert.assertEquals;
 public class CourseRepositoryShould {
 
     @Test
-    public void persist_new_course() {
-        // Arrange
-        List<Teacher> teachers = new ArrayList<Teacher>();
-        teachers.add(Teacher.newInstance("幼幼", "13809878765"));
-        DateRange dateRange = new DateRange("Wednesday", "18:00", "19:30", "2015.02.11", 12);
-        Course courseToBeCreated = Course.newInstance("美术预科",
-                "针对2-3岁儿童心理、生理特点以及敏感期的发展特点，从最基础的看、摸、闻、听、尝（视觉、触觉、嗅觉、听觉、味觉）感觉入手，培养孩子最基础的",
-                "2～3岁",
-                3000, "达芬奇", teachers, dateRange);
-        CourseRepository repository = CourseRepository.newInstance();
-
-        // Act
-        repository.save("美术预科", courseToBeCreated);
-
-        // Assert
-        Course courseRetrieved = repository.retrieve("美术预科");
-        assertEquals(courseToBeCreated, courseRetrieved);
-    }
-
-    @Test
     public void persist_existing_course() {
         // Arrange
         List<Teacher> teachers = new ArrayList<Teacher>();
