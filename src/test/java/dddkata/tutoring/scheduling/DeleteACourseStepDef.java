4a83b8277037058d7c31d5a7c4d4a4c9f22ab532
Ben Wu <wubin28@gmail.com>
2015-07-27 11:01:55 +0800

    Created missing step definition Java classes.

---
+++ src/test/java/dddkata/tutoring/scheduling/DeleteACourseStepDef.java
@@ -0,0 +1,36 @@
+package dddkata.tutoring.scheduling;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by twer on 7/27/15.
+ */
+public class DeleteACourseStepDef {
+    @Given("^the Preparatory course has an enrolment item$")
+    public void the_Preparatory_course_has_an_enrolment_item() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^delete the course$")
+    public void delete_the_course() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^the course could not be deleted$")
+    public void the_course_could_not_be_deleted() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @And("^the message 'Some kids have enrolled the course. In order to delete the course, you have to delete all its enrolment items first.'$")
+    public void the_message_Some_kids_have_enrolled_the_course_In_order_to_delete_the_course_you_have_to_delete_all_its_enrolment_items_first_() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+}
